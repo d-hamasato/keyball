b33a8e7bce4950f3308e319c7ea87fe57413c4a0
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2023-05-03 18:25:32 +0900

    remove RGB animation

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/d-hamasato/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/d-hamasato/config.h
@@ -20,20 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-// reduced compile size
-// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #    define RGBLIGHT_EFFECT_SNAKE
-// #    define RGBLIGHT_EFFECT_KNIGHT
-// #    define RGBLIGHT_EFFECT_CHRISTMAS
-// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #    define RGBLIGHT_EFFECT_RGB_TEST
-// #    define RGBLIGHT_EFFECT_ALTERNATING
-// #    define RGBLIGHT_EFFECT_TWINKLE
-#endif
-
 #define TAP_CODE_DELAY 5
 
 // https://docs.qmk.fm/#/feature_pointing_device?id=pointing-device-auto-mouse
